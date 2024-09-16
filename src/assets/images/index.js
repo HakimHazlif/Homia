d3c76ff4fd8d142dd243499377f143b8d505128f
hakim <hakimhazlif@gmail.com>
2024-09-17 00:45:10 +0100

    Added sample images to assets/images

---
+++ src/assets/images/index.js
@@ -0,0 +1,20 @@
+import bedroom from './bedroom.jpeg'
+import diningRoom from './dining-room.jpg'
+import kitchen from './kitchen.jpeg'
+import livingRoom from './living-room.jpeg'
+import happyCouple from './happy-couple.jpg'
+import happyFamily from './happy-family.jpeg'
+import happyFriends from './happy-friends.jpg'
+import happyLivingRoom from './happy-living-room.jpg'
+
+
+export {
+    bedroom,
+    diningRoom,
+    kitchen,
+    livingRoom,
+    happyCouple,
+    happyFamily,
+    happyFriends,
+    happyLivingRoom
+}
